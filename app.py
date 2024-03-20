417e8179a1b7acb2b5d81e5413de1cc4c4ccbdf2
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-03-19 17:56:30 -0700

    add attempted notes, fix sanity check, bugs

--- app.py
+++ app.py
@@ -16,13 +16,19 @@ def post():
 
     # remove all the arrays that only have 1 value
     for k in res:
-        if (k not in ('attempted_notes', "shot_locations")):
+        if (k not in ('attempted_notes', "shot_locations_auton", "shot_locations_teleop")):
             res[k] = res[k][0];
             # if (res[k].isnumeric()):
             #     res[k] = int(res[k]);
             # elif res[k] == "on":
             #     res[k] = True;
     print("saving:", res);
+
+    if (int(res["autoNotesScored"]) > int(res["autoNotesAttempted"])):
+        return json.dumps({"message": "Auton Notes Scored > Auton Notes Attempted! This is impossible"}), 400
+
+    if (int(res["teleNotesScored"]) > int(res["teleNotesAttempted"])):
+        return json.dumps({"message": "Teleop Notes Scored > Teleop Notes Attempted! This is impossible"}), 400
 
     team_folder = data / res["team_number"] / ("qual" if res["is_qual"] == "on" else "playoff");
     team_folder.mkdir(parents=True, exist_ok=True);
